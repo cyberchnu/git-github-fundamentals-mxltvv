e8ff1822b129a6fd0ec7227bc551cbd28feeba3d
mxltvv <125269421+mxltvv@users.noreply.github.com>
2023-02-18 12:50:15 +0300

    Add files via upload

---
+++ 7-10.py
@@ -0,0 +1,19 @@
+print (6*2)
+print (10+2)
+print ( 14-2)
+print (24//2)
+lybume_chuslo = 777
+message = "Моє улюблене число: " + str(lybume_chuslo)
+print(message)
+favoriteName = "Maximillian"
+print(favoriteName.lower())
+print(favoriteName.upper())
+print(favoriteName.title())
+poem = """Так! я буду крізь сльози сміятись,
+
+Серед лиха співати пісні,
+
+Без надії таки сподіватись,
+
+Буду жити! Геть думи сумні!"""
+print (poem)
