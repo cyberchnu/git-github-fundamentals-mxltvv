f43c967c6425ff3d6624ba088e2871c59cfbe5c2
mxltvv <125269421+mxltvv@users.noreply.github.com>
2023-02-18 12:36:54 +0300

    Add files via upload

---
+++ 1-6.py
@@ -0,0 +1,6 @@
+seconds_per_hour = 60 * 60
+seconds_per_day = 24 * seconds_per_hour
+result1 = seconds_per_day / seconds_per_hour
+#print(result1)
+result2 = seconds_per_day // seconds_per_hour
+print(result2)
